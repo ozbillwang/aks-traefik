7b1d8cd2bd1dd962bf75d90cc2b7d471a36defe5
Bill Wang <bill.wang>
2021-03-21 15:43:25 +1100

    tune

--- README.md
+++ README.md
@@ -1,3 +1,105 @@
+# Deploy application with Traefik to Azure Kubernetes Service (AKS)
+
+This repo is forked from https://github.com/microsoft/mindaro. In original repo, it explains how `Bridge to kubernetes` works. But I do like the way to deploy an application with [Traefik](https://traefik.io/) within Azure Kubernetes Service (AKS)
+
+So I update it with my requirement.
+
+1. Remove the dependency for any Windows usage, such as WSL.
+2. Tuning the scripts, remove the hardcodes.
+
+### Prerequisite
+
+* [install latest azure cli](https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest)
+* [install latest kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/)
+* [install latest helm](https://helm.sh/docs/helm/helm_install/)
+* [Deploy an Azure Kubernetes Service (AKS)](https://docs.microsoft.com/en-us/azure/aks/kubernetes-walkthrough-portal)
+* update aks kube config locally
+
+```
+$ az login
+
+# if you have multiple subscriptions
+$ az account set -s <subscription name>
+
+# merge the AKS kube config to your default ~/.kube/config
+# It is better to backup ~/.kube/config first, before you run below command
+$ az aks get-credentials -g $RGNAME -n $AKSNAME
+```
+
+### Create application "bikeapp"
+
+```
+$ bash ./bridge-quickstart.sh
+
+Bridge to Kubernetes
+Bike Sample App - Quickstart script
+-----------------------------------
+
+Using kubernetes namespace: bikeapp
+
+helm install traefik ingress controller in bikeapp
+Release "bikesharing-traefik-bikeapp" has been upgraded. Happy Helming!
+NAME: bikesharing-traefik-bikeapp
+LAST DEPLOYED: Sun Mar 21 14:56:32 2021
+NAMESPACE: bikeapp
+STATUS: deployed
+REVISION: 13
+TEST SUITE: None
+NOTES:
+1. Get Traefik's load balancer IP/hostname:
+
+     NOTE: It may take a few minutes for this to become available.
+
+     You can watch the status by running:
+
+         $ kubectl get svc bikesharing-traefik --namespace bikeapp -w
+
+     Once 'EXTERNAL-IP' is no longer '<pending>':
+
+         $ kubectl describe svc bikesharing-traefik --namespace bikeapp | grep Ingress | awk '{print $3}'
+
+2. Configure DNS records corresponding to Kubernetes ingress resources to point to the load balancer IP/hostname found in step 1
+
+Waiting for BikeSharing ingress Public IP to be assigned...
+
+BikeSharing ingress Public IP:  20.xx.xxx.xxx
+The Nip.IO FQDN would be  20.xx.xxx.xxx.nip.io
+---
+Chart directory: samples/BikeSharingApp/charts/
+helm install bikesharingapp (average time to install = 4 minutes)
+Hang tight while we grab the latest from your chart repositories...
+...
+Update Complete. ⎈Happy Helming!⎈
+Saving 9 charts
+Deleting outdated charts
+Release "bikesharingapp" has been upgraded. Happy Helming!
+NAME: bikesharingapp
+LAST DEPLOYED: Sun Mar 21 14:57:01 2021
+NAMESPACE: bikeapp
+STATUS: deployed
+REVISION: 2
+TEST SUITE: None
+
+To try out the app, open the url:
+Warning: extensions/v1beta1 Ingress is deprecated in v1.14+, unavailable in v1.22+; use networking.k8s.io/v1 Ingress
+bikeapp.bikesharingweb.20.xx.xxx.xxx.nip.io
+
+```
+
+It takes about 5~10 minutes, then you should be fine to access the bike sharing website via http://bikeapp.bikesharingweb.20.xx.xxx.xxx.nip.io
+
+###  delete the application
+
+```
+kubectl delete ns bikeapp
+```
+
+### Understand how DNS with nip.io works
+
+https://nip.io/
+
+Below is the original README.
+
 # Bridge to Kubernetes
  
 Bridge to Kubernetes extends the Kubernetes perimeter to your development computer allowing you to write, test, and debug microservice code while connected to your Kubernetes cluster with the rest of your application or services. With this workflow, there is no need for extra assets, such as a Dockerfile or Kubernetes manifests. You can simply run your code natively on your development workstation while connected to the Kubernetes cluster, allowing you to test your code changes in the context of the larger application.
--- bridge-quickstart.sh
+++ bridge-quickstart.sh
@@ -1,142 +1,38 @@
 #!/bin/bash
 set -e
 
-# Please install the below pre-requisites if using this on your local machine, or alternately, you can just use Azure Cloud Shell for a seamless experience.
-#1. azure cli    :   https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
-#2. kubectl      :   https://kubernetes.io/docs/tasks/tools/install-kubectl/
-#3. curl
-#4. gunzip
-#5. tar
+# Prerequisite
+# azure cli: https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest
+# kubectl: https://kubernetes.io/docs/tasks/tools/install-kubectl/
+# helm: https://helm.sh/docs/helm/helm_install/
+# Deploy an Azure Kubernetes Service (AKS)
+# update aks kube config: az aks get-credentials -g $RGNAME -n $AKSNAME
 
-HELMDIR=/var/tmp/helm_bridge
 INGRESSNAME=bikesharing-traefik
+BIKENS="bikeapp"
+
+# update the version if required.
+TRAEFIK_CHART_VERSION="9.17.5"
 
 echo ""
 echo "Bridge to Kubernetes"
 echo "Bike Sample App - Quickstart script"
 echo "-----------------------------------"
 echo ""
-if [ "$OSTYPE" == "msys" ]; then
-   echo "The script is currently only supported in Windows using WSL. https://aka.ms/wsl"  
-   echo "Alternatively you can use the Azure Cloud Shell https://shell.azure.com (bash)"
-   
-   exit 1
-fi
 
-helpFunction()
-{
-   echo ""
-   echo "Usage: $1 -g ResourceGroupName -n AKSName"
-   echo -e "\t-g Name of resource group of AKS Cluster"
-   echo -e "\t-n Name of AKS Cluster"
-   echo -e "\t-k Kubernetes namespace (default = bikeapp)"
-   echo -e "\t-r Path to Root of the git repo (default = pwd)"
-   echo -e "\t-c Cleanup"
-   echo -e "\t-d Helm Debug switch"
-   exit 1 # Exit script after printing help
-}
-
-installHelmFunction()
-{
-  if [ ! -d "$HELMDIR" ]; then
-    mkdir $HELMDIR
-    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-       curl -fsSL -o $HELMDIR/helm.tar.gz https://get.helm.sh/helm-v3.2.1-linux-amd64.tar.gz
-       gunzip -c $HELMDIR/helm.tar.gz | tar xopf - -C $HELMDIR
-       mv $HELMDIR/linux-amd64/helm $HELMDIR
-    elif [[ "$OSTYPE" == "darwin"* ]]; then
-       curl -fsSL -o $HELMDIR/helm.tar.gz https://get.helm.sh/helm-v3.2.1-darwin-amd64.tar.gz
-       gunzip -c $HELMDIR/helm.tar.gz | tar xopf - -C $HELMDIR
-       mv $HELMDIR/darwin-amd64/helm $HELMDIR
-    else
-       echo "OS not recognized. Please either run on linux-gnu or osx"
-       exit 1
-    fi
-  fi
-}
-
-cleanupFunction()
-{
-   set +e
-   echo ""
-   echo "Setting the Kube context to $AKSNAME in $RGNAME"
-   az aks get-credentials -g $RGNAME -n $AKSNAME
-   $HELMDIR/helm --namespace $BIKENS uninstall bikesharingapp
-   $HELMDIR/helm --namespace $BIKENS uninstall $INGRESSNAME
-   echo "Delete namespace $BIKENS? (Y/n): "
-   read RESPONSE
-   if [ "$RESPONSE" == "y" ] || [ "$RESPONSE" == "Y" ]; then
-      kubectl delete namespace $BIKENS
-   fi
-   rm -rf $HELMDIR
-   exit 0
-}
-
-while getopts "g:n:r:k:cd" opt; do
-   case "$opt" in
-      c ) CLEANUP="true"  ;;
-      g ) RGNAME="$OPTARG"  ;;
-      n ) AKSNAME="$OPTARG"  ;;
-      r ) REPOROOT="$OPTARG"  ;;
-      d ) HELMARGS=" --debug" ;;
-      k ) BIKENS="$OPTARG" ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-   esac
-done
-
-if [ -z "$BIKENS" ]; then
-   BIKENS="bikeapp"
-fi
-BIKENS=$(echo $BIKENS | tr [:upper:] [:lower:])
 echo "Using kubernetes namespace: $BIKENS"
 
-# Print helpFunction in case parameters are empty
-if [ ! -z "$CLEANUP" ]; then
-   echo ""
-   echo "Running app Cleanup"
-   if [ -z "$RGNAME" ]; then
-      echo "Please pass -g when calling -c"
-      helpFunction
-   else
-      cleanupFunction
-   fi
-elif [ -z "$RGNAME" ] || [ -z "$AKSNAME" ]; then
-   echo "Some or all of the parameters are empty";
-   helpFunction
-fi
-
-if [ -z "$REPOROOT" ]; then
-   echo "Defaulting Git repository root to current directory: $PWD"
-   REPOROOT="$PWD"
-fi
-
-echo "Checking directory $REPOROOT for GIT repo Microsoft/Mindaro"
-if [ ! -d "$REPOROOT/samples/BikeSharingApp" ]; then
-  echo "$(tput setaf 1)ERROR: BikeSharingApp not found in $REPOROOT/samples/BikeSharingApp$(tput sgr 0)"
-  echo "Did you download the git repo?"
-  echo "Run; git clone https://github.com/microsoft/mindaro.git [local-repo-path]"
-  exit 1
-fi
-
-installHelmFunction
-
-echo "Setting the Kube context to $AKSNAME in $RGNAME"
-az aks get-credentials -g $RGNAME -n $AKSNAME
-
-# Use Helm to deploy a traefik ingress controller
-echo "helm repo add && helm repo update"
-$HELMDIR/helm repo add stable https://charts.helm.sh/stable
-$HELMDIR/helm repo update
-
 echo ""
-echo "helm install traefik ingress controller in $BIKENS $HELMARGS"
-$HELMDIR/helm install "$INGRESSNAME-$BIKENS" stable/traefik \
+echo "install traefik ingress controller in $BIKENS"
+helm repo add traefik https://helm.traefik.io/traefik
+helm repo update
+helm upgrade --install "$INGRESSNAME-$BIKENS" traefik/traefik \
    --namespace $BIKENS --create-namespace \
    --set kubernetes.ingressClass=traefik \
    --set fullnameOverride=$INGRESSNAME \
    --set rbac.enabled=true \
    --set kubernetes.ingressEndpoint.useDefaultPublishedService=true \
-   --version 1.85.0 $HELMARGS
+   --version ${TRAEFIK_CHART_VERSION}
 
 echo ""
 echo "Waiting for BikeSharing ingress Public IP to be assigned..."
@@ -144,28 +40,27 @@ while [ -z "$PUBLICIP" ]; do
   sleep 5
   PUBLICIP=$(kubectl get svc -n $BIKENS $INGRESSNAME -o jsonpath={.status.loadBalancer.ingress[0].ip})
 done
+
 echo ""
 echo "BikeSharing ingress Public IP: " $PUBLICIP
 
-NIPIOFQDN=$PUBLICIP.nip.io
-echo "The Nip.IO FQDN would be " $NIPIOFQDN
+NIPIOFQDN="$PUBLICIP.nip.io"
+echo "The Nip.IO FQDN would be $NIPIOFQDN"
  
-CHARTDIR="$REPOROOT/samples/BikeSharingApp/charts/"
+CHARTDIR="samples/BikeSharingApp/charts/"
 echo "---"
 echo "Chart directory: $CHARTDIR"
 
-
-
-echo "helm install bikesharingapp (average time to install = 4 minutes)"
-$HELMDIR/helm install bikesharingapp "$CHARTDIR" \
+echo "install bikesharingapp (average time to install = 4 minutes)"
+helm dependency update "$CHARTDIR" 
+helm upgrade --install bikesharingapp "$CHARTDIR" \
    --set bikesharingweb.ingress.hosts={$BIKENS.bikesharingweb.$NIPIOFQDN} \
    --set gateway.ingress.hosts={$BIKENS.gateway.$NIPIOFQDN} \
    --set bikesharingweb.ingress.annotations."kubernetes\.io/ingress\.class"=traefik \
    --set gateway.ingress.annotations."kubernetes\.io/ingress\.class"=traefik \
-   --dependency-update \
    --namespace $BIKENS \
    --timeout 9m \
-   --atomic $HELMARGS
+   --atomic
 
 echo ""
 echo "To try out the app, open the url:"
